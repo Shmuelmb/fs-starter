151afcad0af501d7b692e5394a5439f5de0c7f64
shmuelmb <>
2025-01-08 20:54:05 +0200

    init

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import './lib/module-alias-config'
+import express, { Request, Response, Application } from 'express'
+import dotenv from 'dotenv'
+import { test } from '@/lib/utils'
+
+//For env File
+dotenv.config()
+const app: Application = express()
+const port = process.env.PORT || 8000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server')
+})
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`)
+})
+
+test()
