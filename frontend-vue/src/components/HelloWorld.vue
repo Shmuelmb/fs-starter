49f5ab6609c8a00b7f1660ae3d5fc5632848f2fa
shmuelmb <>
2025-01-09 09:15:39 +0200

    added vue starter

---
+++ frontend-vue/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+</template>
+
+<style scoped>
+h1{
+  text-align: center;
+  width: 100vw;
+}
+</style>
